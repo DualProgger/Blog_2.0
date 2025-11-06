ff8bdae801109507ae13e607debc8aaba89c8976
Dmitry_work_ubuntu <tcheprasov.mitya@yandex.ru>
2025-11-06 14:46:31 +0300

    Create Blog 2.0

---
+++ apps/blog/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django_mptt_admin.admin import DjangoMpttAdmin
+from .models import Post, Category
+
+@admin.register(Category)
+class CategoryAdmin(DjangoMpttAdmin):
+    """
+    Админ-панель модели категорий
+    """
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Админ-панель модели записей
+    """
+    prepopulated_fields = {'slug': ('title',)}
---
+++ apps/blog/views.py
@@ -0,0 +1,51 @@
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from .models import Post, Category
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+    queryset = Post.custom.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        page = context['page_obj']
+        context['paginator_range'] = page.paginator.get_elided_page_range(page.number)
+        return context
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # заголовок получаем из заголовка статьи
+        context['title'] = self.object.title
+        return context
+
+
+class PostFromCategory(ListView):
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    category = None
+    paginate_by = 1
+
+    def get_queryset(self):
+        self.category = Category.objects.get(slug=self.kwargs['slug'])
+        queryset = Post.custom.filter(category=self.category)
+        if not queryset:
+            sub_cat = Category.objects.filter(parent=self.category)
+            queryset = Post.custom.filter(category__in=sub_cat)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Записи из категории: {self.category.title}'
+        page = context['page_obj']
+        context['paginator_range'] = page.paginator.get_elided_page_range(page.number)
+        return context
