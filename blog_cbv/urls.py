c1b12a1b1411c5697617a14048a7902b33632a60
Dmitry_work_ubuntu <tcheprasov.mitya@yandex.ru>
2025-11-08 15:13:31 +0300

    Add apps Accounts

--- blog_cbv/urls.py
+++ blog_cbv/urls.py
@@ -22,7 +22,8 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.blog.urls'))
+    path('', include('apps.blog.urls')),
+    path('', include('apps.accounts.urls')),
 ]
 
 if settings.DEBUG:
